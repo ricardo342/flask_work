3f3abc9a7106595dc139ef0b649c96284682ca40
majiaqin <majiaqin@techb.top>
2022-07-12 19:10:09 +0800

    新增模板1

--- Flash.py
+++ Flash.py
@@ -8,13 +8,32 @@
 '''
 from flask import Flask, flash, redirect, render_template, \
 request, url_for
+from werkzeug.utils import secure_filename
+from flask_mail import Mail, Message
+from flask_wtf import Form
+from wtforms import TextAreaField
+import os
 
 app = Flask(__name__)
 app.secret_key = 'random string'
+app.config['UPLOAD_FOLDER'] = 'upload/'
+Mail(app=None)
+
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USERNAME'] = 'yourId@gmail.com'
+app.config['MAIL_PASSWORD'] = '*****'
+app.config['MAIL_USE_TLS'] = False
+app.config['MAIL_USE_SSL'] = True
+mail = Mail(app)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    msg = Message('Hello', sender='yourId@gmail.com',
+                  recipients=['id1@gmail.com'])
+    msg.body = 'This is the email body'
+    mail.send(msg)
+    return 'Sent'
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -28,6 +47,28 @@ def login():
             return redirect(url_for('index'))
 
     return render_template('login.html', error=error)
+
+@app.route('/upload')
+def upload_file():
+    return render_template('upload.html')
+
+@app.route('/uploader', methods=['GET', 'POST'])
+def uploader():
+    if request.method == 'POST':
+        f = request.files['file']
+        print(request.files)
+        f.save(os.path.join(app.config['UPLOAD_FOLDER'], secure_filename(f.filename)))
+        return 'file uploaded successfully'
+    else:
+        return render_template('upload.html')
+
+class ContactForm(Form):
+    name = TextAreaField("Name Of Student")
+
+@app.route('/contact')
+def contact():
+    form = ContactForm()
+    return render_template('contact.html', form=form)
 
 if __name__ == '__main__':
     app.run(debug=True)
